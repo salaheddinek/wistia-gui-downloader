de38cf764d37f26d8af0deb47a301fcc50252938
salaheddinek <salaheddinek@protonmail.ch>
2022-07-02 14:05:32 +0200

    Replaced QMessageBox with QDialog

--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-__version__ = '2.1.3'
+__version__ = '2.2.1'
 __author__ = 'Salah Eddine Kabbour'
 __package__ = "wistia_downloader"
 
@@ -292,24 +292,46 @@ class MainWindow(Qw.QMainWindow):
         text += config.HELP_TEXT
         self.window_message(text, "About page")
 
-    def window_message(self, msg, title="Info", w=800, h=500):
+    def window_message(self, msg, title="Info", minimum_width=500):
         txt = msg.replace("<h6>", "&lt;h6&gt;")
         txt = txt.replace("</h6>", "&lt;/h6&gt;")
         txt = txt.replace("\n", "<br/>")
 
-        msg_box = Qw.QMessageBox(self)
-        msg_box.setPalette(self.palette())
-        msg_box.setFont(self.font())
-        msg_box.setWindowTitle(title)
-        msg_box.setText(txt)
-        msg_box.show()
+        used_font = self.font().__copy__()
+        used_size = used_font.pointSize()
+        if used_size > 2:
+            used_font.setPointSize(used_size - 2)
 
-        btn = msg_box.findChild(Qw.QPushButton)
-        btn.setFont(self.font())
+        qd = Qw.QDialog(self)
+        qd.setModal(True)
+        qd.setPalette(self.palette())
+        qd.setWindowTitle(title)
+
+        layout = Qw.QVBoxLayout()
+        label = Qw.QLabel(txt, qd)
+        label.setTextFormat(Qc.Qt.RichText)
+        label.setWordWrap(True)
+        label.setPalette(self.palette())
+        label.setFont(used_font)
+        layout.addWidget(label)
+        mini_layout = Qw.QHBoxLayout()
+        h_spacer = Qw.QSpacerItem(40, 20, Qw.QSizePolicy.Policy.Expanding, Qw.QSizePolicy.Policy.Minimum)
+        mini_layout.addSpacerItem(h_spacer)
+        btn = Qw.QPushButton(qd)
         btn_color = btn.palette().color(Qg.QPalette.Button).name()
         btn.setStyleSheet(styling.generate_button_stylesheet(btn_color))
-        msg_box.setStyleSheet(f"QLabel {{min-width: {w}px; min-height: {h}px;}}")
-        msg_box.exec()
+        btn.setFont(used_font)
+        btn.setMinimumWidth(btn.minimumWidth())
+        btn.clicked.connect(qd.accept)
+        btn.setText("   OK   ")
+        mini_layout.addWidget(btn)
+        mini_layout.addSpacerItem(h_spacer)
+        layout.addLayout(mini_layout)
+        qd.setMinimumWidth(minimum_width)
+
+        qd.setLayout(layout)
+        qd.show()
+        qd.exec()
 
     @Qc.Slot()
     def log(self, msg, log_lvl=logging.INFO, time=5000):
